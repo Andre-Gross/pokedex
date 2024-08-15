20e3bb44c0a814988303979522519cf94beb3fa7
Andre Gross <andre.handy94@gmail.com>
2024-08-15 22:59:00 +0200

    added some styles, type-specific background-colours and basic design of pokeCard, load more overviewCards now

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let pokeID = 1
-;
-let amountOfCards = 20;
+let pokeID = 1;
+let amountOfCards = 100;
 let firstVisibleCard = 0
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 const FORM_URL = "-form"
 
 
 async function init() {
-    let overviewData = await loadOverviewData(pokeID);
     let container = document.getElementById("container")
-    
-    container.innerHTML = overviewCardHTML(overviewData, pokeID);
-    addTypesHTML(overviewData, pokeID);
+
+    container.innerHTML = "";
+
+    for (let id = pokeID; id < pokeID + amountOfCards; id++) {
+        let overviewData = await loadOverviewData(id);
+        container.innerHTML += await overviewCardHTML(overviewData, id);
+        addTypesHTML(overviewData, id);
+        addBackgroundColour(overviewData, id);
+    }
 }
 
 
@@ -28,13 +32,17 @@ function upperCaseFirstLetter(word) {
 }
 
 
-function overviewCardHTML(overviewData, id){
+async function overviewCardHTML(overviewData, id) {
     let name = upperCaseFirstLetter(overviewData.name);
+    let sprite = await overviewData.sprites.front_default;
 
     let pokeCard = `
-        <div>
+        <div id="pokeCardNo${id}" class="pokeCard rounded">
             <h3>${name}</h3>
-            <div id=types${id}><div> 
+            <div class="d-flex">
+                <div id=typesOf${id}></div> 
+                <img src="${sprite}" alt="${name}">
+            </div>
         </div>
     `
 
@@ -43,15 +51,19 @@ function overviewCardHTML(overviewData, id){
 
 
 function addTypesHTML(overviewData, id) {
-    let typeContainer = document.getElementById(`types${id}`)
+    let typeContainer = document.getElementById(`typesOf${id}`)
     let type0 = upperCaseFirstLetter(overviewData.types[0].type.name);
-    console.log(type0);
 
     typeContainer.innerHTML = `<div>${type0}</div>`
 
-    if(overviewData.types.length > 1){
+    if (overviewData.types.length > 1) {
         let type1 = upperCaseFirstLetter(overviewData.types[1].type.name);
         typeContainer.innerHTML += `<div>${type1}</div>`;
     }
+}
+
+
+function addBackgroundColour(overviewData, id) {
+    document.getElementById(`pokeCardNo${id}`).classList.add(`bc-${overviewData.types[0].type.name}`);
 }
 
